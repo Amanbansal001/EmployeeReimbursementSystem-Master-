a4c5ecc202c0804f55c1ad37ad42bea9158e05d9
hp <Amanbansal001>
2020-10-24 13:07:50 +0530

    Test

--- EmployeeReimbursementSystem/src/main/java/com/employeeReimbursementSystem/controller/UserController.java
+++ EmployeeReimbursementSystem/src/main/java/com/employeeReimbursementSystem/controller/UserController.java
@@ -1,5 +1,15 @@
 package com.employeeReimbursementSystem.controller;
 
-public class UserController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+public class UserController {
+	
+	@RequestMapping("/userDashboard")
+	public String displayHome()
+	{
+		return "userDashboard";
+	}
+	
 }
